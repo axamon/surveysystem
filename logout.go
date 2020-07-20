f9cc58d24f7d4a733d8498f86664d22d9894ef53
Alberto Bregliano <alberto.bregliano@pm.me>
2020-07-20 12:40:35 +0200

    logout works

--- logout.go
+++ logout.go
@@ -3,23 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 )
 
 func logout(w http.ResponseWriter, r *http.Request) {
+	var logoutTmpl = template.Must(template.ParseFiles("templates/logout2.gohtml", "templates/footer.gohtml"))
+
 	session, err := store.Get(r, "surveyCTIO")
 	if err != nil {
 		log.Println(err)
+	} else {
+		// Revoke users authentication
+		session.Values["authenticated"] = false
+		session.Options.MaxAge = -1
+		err = session.Save(r, w)
+		if err != nil {
+			log.Println(err)
+		}
+		if _, ok := session.Values["utente"]; ok {
+			log.Printf("Logout effettuato per %s", session.Values["utente"].(string))
+		}
+
 	}
-	// Revoke users authentication
-	session.Values["authenticated"] = false
-	session.Options.MaxAge = -1
-	err = session.Save(r, w)
+	// w.Write([]byte("Logout effettuato\n"))
+	// time.Sleep(2 * time.Second)
+	err = logoutTmpl.Execute(w, nil)
 	if err != nil {
 		log.Println(err)
 	}
-	log.Printf("Logout effettuato per %s", session.Values["utente"].(string))
-	// w.Write([]byte("Logout effettuato\n"))
-	// time.Sleep(2 * time.Second)
-	r.Method = "GET"
-	http.Redirect(w, r, "/", http.StatusFound)
 }
